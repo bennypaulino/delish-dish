7d03f37176944373fbc53ac3a3d0a9eb43b38e3b
bennypaulino <santiagopro71@gmail.com>
2017-03-28 09:42:34 -0400

    Refactor and DRY up the code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order('created_at DESC')
   end
@@ -13,39 +15,39 @@ class PostsController < ApplicationController
       flash[:success] = "That looks like a delicious dish!"
       redirect_to @post
     else
-      flash.now[:alert] = 'Forgetting something? You need an image to post here!'
+      flash.now[:alert] = "Your DelishDish couldn't be created, please check the form."
       render :new
     end
   end
 
   def show
-    @post = Post.find_by_id(params[:id])
   end
 
   def edit
-    @post = Post.find_by_id(params[:id])
   end
 
   def update
-    @post = Post.find_by_id(params[:id])
     @post.update_attributes(post_params)
     if @post.valid?
       flash[:success] = 'Dish updated!'
       redirect_to(post_path(@post))
     else
-      flash.now[:alert] = "Something is wrong with your form, check it again..."
+      flash.now[:alert] = "Something is wrong with your form, please check it again..."
       render :edit
     end
   end
 
   def destroy
-    @post = Post.find_by_id(params[:id])
     @post.destroy
     redirect_to root_path
     flash[:success] = 'Problem solved! Dish deleted.'
   end
 
   private
+
+  def set_post
+    @post = Post.find_by_id(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:caption, :image)
