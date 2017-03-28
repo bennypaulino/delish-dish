cd995247ce6a0be0b81aeb551f02511f676a9349
bennypaulino <santiagopro71@gmail.com>
2017-03-28 13:53:49 -0400

    Add user_id column to posts & associate models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   validates :image, presence: :true
   has_attached_file :image, styles: { medium: "640x" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  belongs_to :user
 end
