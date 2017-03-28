4135cd72a75bda24b51509fa2d667fd4dd89282f
bennypaulino <santiagopro71@gmail.com>
2017-03-28 13:07:45 -0400

    Address failing unit & feature tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    email 'big@lebowski.com'
+    user_name 'dude'
+    password 'nicemarmot'
+    id 1
   end
 end
--- spec/features/creating_new_posts_spec.rb
+++ spec/features/creating_new_posts_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper.rb'
 
 feature 'Creating posts' do
-  scenario 'can create a post' do
+  background do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
     visit '/'
     click_link 'New Post'
+  end
+
+  scenario 'can create a post' do
     attach_file('Image', "spec/files/images/padthai.jpg")
     fill_in 'Caption', with: 'yum-yum, #noodletime'
     click_button 'Create Post'
@@ -12,8 +17,6 @@ feature 'Creating posts' do
   end
 
   it 'needs an image to create a post' do
-    visit '/'
-    click_link 'New Post'
     fill_in 'Caption', with: 'Nothing to see here'
     click_button 'Create Post'
     expect(page).to have_content("Your DelishDish couldn't be created, please check the form.")
