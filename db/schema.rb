ea1db306aaee4af614fe59f778aa549f7684fc11
bennypaulino <santiagopro71@gmail.com>
2017-03-28 17:23:45 -0400

    Add title column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328185921) do
+ActiveRecord::Schema.define(version: 20170328211615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170328185921) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "user_id"
+    t.string   "title"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
