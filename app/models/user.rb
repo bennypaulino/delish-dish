8d0c42f542e16efaa4d713a79a5c6192479ac884
bennypaulino <santiagopro71@gmail.com>
2017-03-28 12:11:41 -0400

    Adjust User model & get its unit test to pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  validates :email, uniqueness: {case_sensitive: false}
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+
+  validates :user_name, presence: true, uniqueness: {case_sensitive: false}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
