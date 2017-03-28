8bbb65870b1c3d4a7631f7b47b937644329d0b45
bennypaulino <santiagopro71@gmail.com>
2017-03-28 13:45:26 -0400

    Adjust navbar & ensure tests pass

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -10,10 +10,11 @@ feature 'User authentication' do
     expect(page).to_not have_content('New Post')
 
     click_link 'Login'
-    fill_in 'Email', with: 'glassyeyes@hypnotoad.com'
-    fill_in 'Password', with: 'gettingsleepy'
+    fill_in 'Email', with: 'big@lebowski.com'
+    fill_in 'Password', with: 'nicemarmot'
 
     click_button 'Log in'
+    # puts page.html
     expect(page).to have_content('Signed in successfully')
     expect(page).to_not have_content('Register')
     expect(page).to have_content('Logout')
