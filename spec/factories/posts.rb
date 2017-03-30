921f963a1cf28a7f9e338870190bd0ff92dce1e0
bennypaulino <santiagopro71@gmail.com>
2017-03-30 15:02:44 -0400

    Write unit tests for Post model

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :post do
+    title "Photo skills"
     caption "nofilter"
     image Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/padthai.jpg', 'image/jpg')
   end
